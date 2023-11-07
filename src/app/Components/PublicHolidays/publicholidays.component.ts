4647434a8adf2c111530a7a3d22311c52d26c5f1
Karim Mahdy <karimmahdy43@gmail.com>
2023-11-07 16:08:57 +0200

    update publicholidays

--- src/app/Components/PublicHolidays/publicholidays.component.ts
+++ src/app/Components/PublicHolidays/publicholidays.component.ts
@@ -27,12 +27,14 @@ export class PublicholidaysComponent implements OnInit {
     name: new FormControl('', [
       Validators.required,
       Validators.maxLength(30),
-      Validators.minLength(2),
-    ]),
+      Validators.minLength(2),]),
     data: new FormControl('', [
       Validators.required,
     ])
   });
+  get controlsname() {
+    return this.PublicHolidyFrom.controls;
+  }
   ngOnInit(): void {
     this.publicholidaysService.GetAllPublicholidys().subscribe({
       next: (response) => {
@@ -46,4 +48,77 @@ export class PublicholidaysComponent implements OnInit {
     
   }
 
+  OnSubmit(e: Event) {
+    e.preventDefault();
+    this.submitted = true;
+    if (this.PublicHolidyFrom.valid) {
+      if (this.publicHolidayId != undefined) {
+        this.publicholidaysService.EditPublicholidy(this.PublicHolidyFrom.value, this.publicHolidayId).subscribe({
+          next: (res) => {
+            this.publicholidaysService.GetAllPublicholidys().subscribe({
+              next: (response) => {
+                this.publicHolidays = response;
+                this.dtTrigger.next(null);
+              },
+            })
+          },
+          error:(error:any)=>{  
+            this.ModelState = error.error;
+          }
+        });
+      }
+      else {
+        this.publicholidaysService.AddPublicholidy(this.PublicHolidyFrom.value).subscribe({
+          next: () => {
+            this.publicholidaysService.GetAllPublicholidys().subscribe({
+              next: (response) => {
+                this.publicHolidays = response;
+                this.dtTrigger.next(null);
+              },
+            })
+          },
+          error:(error:any)=>{  
+            this.ModelState = error.error;
+          }
+        });
+      }
+    } 
+  }
+
+  OnEdit(id:any){
+    console.log(id);
+    
+    this.publicHolidayId = id;
+    this.publicholidaysService.GetPublicholidyId(id).subscribe({
+      next: (response:any) => {
+        console.log(response);
+       this.PublicHolidyFrom.controls['id'].setValue(response.id);
+        this.PublicHolidyFrom.controls['name'].setValue(response.name);
+        this.PublicHolidyFrom.controls['data'].setValue(response.data);
+      },
+     
+      
+    })
+    console.log( this.PublicHolidyFrom);
+  }
+
+  OnDelete(id:any){
+    this.publicholidaysService.DeletePublicholidy(id).subscribe({
+      next: () => {
+        this.publicholidaysService.GetAllPublicholidys().subscribe({
+          next: (response) => {
+            this.publicHolidays = response;
+            
+          },
+        })
+      },
+    })
+  }
+
+  OnReset() {
+        this.PublicHolidyFrom.controls['id'].setValue(0);
+        this.PublicHolidyFrom.controls['name'].setValue('');
+        this.PublicHolidyFrom.controls['data'].setValue('');
+        this.publicHolidayId = 0;
+  }
 }
